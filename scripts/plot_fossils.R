b56d285c3230326b25590d6c7212bcafcf7f32ac
Arthur-Boddaert <124796061+Arthur-Boddaert@users.noreply.github.com>
2024-03-27 10:53:10 +0100

    Enhance the model

--- scripts/plot_fossils.R
+++ scripts/plot_fossils.R
@@ -43,7 +43,7 @@ pbdb = pbdb %>% dplyr::mutate(y=1:nrow(pbdb))
 plot_count = pbdb %>% ggplot(aes(x=max_ma, y=y, xend=min_ma, yend=y)) + geom_segment(size=3) + 
   xlim(0, max_time) + ylim(0, max_id) + theme_void()
 
-#ggsave(sprintf('%s_count.png',simul_name), width = size_graph, height = size_graph, units = "cm", bg='white')
+ggsave(sprintf('%s_count.png',simul_name), width = size_graph, height = size_graph, units = "px", bg='white')
 
 # demes
 infile = sprintf('simulated_fossils_%s.txt',simul_name)
@@ -63,8 +63,8 @@ for(deme in 0:nb_demes){
     scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_id_by_deme) + theme_void()
   assign(name, plot_deme)
   
-  #file_name = sprintf('%s_D%s.png', simul_name, deme)
-  #ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+  file_name = sprintf('%s_D%s.png', simul_name, deme)
+  ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 }
 infile = sprintf('df_to_plot_%s.csv', simul_name)
 data = read.csv(infile, h=T, sep = '\t')
@@ -74,55 +74,55 @@ data = data %>% replace(is.na(.), 0)
 plot_mspd = data %>% ggplot(aes(x=age_Ma, y=mspd, xend=age_Ma, yend=mspd)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_mean_sp_by_deme) + theme_void()
 
-#file_name = sprintf('%s_mspd.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_mspd.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
 plot_vspd = data %>% ggplot(aes(x=age_Ma, y=vspd, 
                                 xend=age_Ma, yend=vspd)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_var_sp_by_deme) + theme_void()
 
-#file_name = sprintf('%s_vspd.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_vspd.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
 plot_mgd = data %>% ggplot(aes(x=age_Ma, y=mgd, 
                                xend=age_Ma, yend=mgd)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_mean_genera_by_deme) + theme_void()
 
-#file_name = sprintf('%s_mgd.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_mgd.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
 plot_vgd = data %>% ggplot(aes(x=age_Ma, y=vgd, 
                                xend=age_Ma, yend=vgd)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_var_genera_by_deme) + theme_void()
 
-#file_name = sprintf('%s_vgd.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_vgd.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
 plot_msp = data %>% ggplot(aes(x=age_Ma, y=msp, 
                                xend=age_Ma, yend=msp)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_mean_sp_by_genera) + theme_void()
 
-#file_name = sprintf('%s_msp.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_msp.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
 plot_vsp = data %>% ggplot(aes(x=age_Ma, y=vsp, 
                                xend=age_Ma, yend=vsp)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_var_sp_by_genera) + theme_void()
 
-#file_name = sprintf('%s_vsp.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_vsp.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
 plot_ng = data %>% ggplot(aes(x=age_Ma, y=ng, 
                               xend=age_Ma, yend=ng)) + geom_line()  +
   scale_color_viridis_d() + xlim(0, max_time) + ylim(0, max_genera) + theme_void()
 
-#file_name = sprintf('%s_ng.png',simul_name)
-#ggsave(file_name, width = size_graph, height = size_graph, units = "cm", bg='white')
+file_name = sprintf('%s_ng.png',simul_name)
+ggsave(file_name, width = size_graph, height = size_graph, units = "px", bg='white')
 
-P_final = ggarrange(plot_count, plot_deme_0, plot_deme_1, plot_deme_2, plot_deme_3,
-                    plot_deme_4, plot_deme_5, plot_deme_6, plot_deme_7, plot_deme_8,
-                    plot_deme_9, plot_msp, plot_vsp, plot_mspd, plot_vspd, plot_mgd,
-                    plot_vgd, plot_ng, ncol=6, nrow=3, align = 'hv', common.legend = F)
+#P_final = ggarrange(plot_count, plot_deme_0, plot_deme_1, plot_deme_2, plot_deme_3,
+#                    plot_deme_4, plot_deme_5, plot_deme_6, plot_deme_7, plot_deme_8,
+#                    plot_deme_9, plot_msp, plot_vsp, plot_mspd, plot_vspd, plot_mgd,
+#                    plot_vgd, plot_ng, ncol=6, nrow=3, align = 'hv', common.legend = F)
 
-file_name = sprintf('%s_all.png',simul_name)
-ggsave(file_name, width = size_graph*6, height = size_graph*3, units = "cm", bg='white', limitsize = FALSE)
+#file_name = sprintf('%s_all.png',simul_name)
+#ggsave(file_name, width = size_graph*6, height = size_graph*3, units = "px", bg='white', limitsize = FALSE)
--- scripts/simulation_launcher.py
+++ scripts/simulation_launcher.py
@@ -1,5 +1,5 @@
 import argparse
-from simulation import simulation
+from simulation_V2 import simulation
 
 if __name__ == "__main__":
     parser= argparse.ArgumentParser()
@@ -8,11 +8,15 @@ if __name__ == "__main__":
     parser.add_argument('--proba_E3', '-e3', help='Rates of extinction E3', type=float, required=True, action='store')
     parser.add_argument('--proba_D1', '-d1', help='Rates of speciation D1', type=float, required=True, action='store')
     parser.add_argument('--proba_D2', '-d2', help='Rates of speciation D2', type=float, required=True, action='store')
+    parser.add_argument('--proba_D3', '-d3', help='Rates of speciation D3', type=float, required=True, action='store')
     parser.add_argument('--proba_C1', '-c1', help='Rates of colonisation C1', type=float, required=True, action='store')
     parser.add_argument('--proportion_E2', '-pe2', help='Proportion of genus mass extincted', type=float,required=True, action='store')
     parser.add_argument('--proportion_E3', '-pe3', help='Proportion of demes mass extincted', type=float,required=True, action='store')
     parser.add_argument('--name', '-n', help='Name of simulation', type=str, required=True, action='store')
     parser.add_argument('--output_dir', '-o', help='Output directory', type=str, required=True, action='store')
+    parser.add_argument('--sigma', '-s', help='Values of sigma for each trait', type=int, nargs='+', required=True)
+    parser.add_argument('--k', '-k', help='Values of k for each trait', type=int, nargs='+', required=True)
+    parser.add_argument('--s_deme_csv', '-sdeme', help='The path of a csv file with each line is a locality and each column the s_deme of a trait in this locality.', type=str,  action='store')
     parser.add_argument('--nb_localities', '-nl', help='Number of localities', type=int, default=10, action='store')
     parser.add_argument('--nb_generations', '-ng', help='Number of generations', type=int, default=600, action='store')
     parser.add_argument('--nb_max_genus', '-nmg', help='Max number of genus', type=int, default=2000, action='store')
@@ -20,6 +24,11 @@ if __name__ == "__main__":
     parser.add_argument('--verbose', '-v', help='Display the generations', action='store_true')
     args = parser.parse_args()
 
-    rates = [args.proba_E1, args.proba_E2, args.proba_E3, args.proba_D1, args.proba_D2, args.proba_C1]
+    rates = [args.proba_E1, args.proba_E2, args.proba_E3, args.proba_D1, args.proba_D2, args.proba_D3, args.proba_C1]
     proportions = [args.proportion_E2, args.proportion_E3]
-    simulation(rate=rates, proportion=proportions, simulation_name=args.name, result_repertory=args.output_dir, verbose=args.verbose, nb_localities=args.nb_localities, nb_generations=args.nb_generations, nb_max_genus=args.nb_max_genus, nb_max_lineages_in_genus=args.nb_max_lineages)
+    sigma = args.sigma
+    k = args.k
+    simulation(rate=rates, proportion=proportions, simulation_name=args.name, result_repertory=args.output_dir,
+               verbose=args.verbose, nb_localities=args.nb_localities, nb_generations=args.nb_generations,
+               sigma=sigma, k=k, nb_max_genus=args.nb_max_genus, nb_max_lineages_in_genus=args.nb_max_lineages,
+               s_deme_csv=args.s_deme_csv)
